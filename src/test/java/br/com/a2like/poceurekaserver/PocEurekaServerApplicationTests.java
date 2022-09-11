1a5f6500d14d50d26d66709d76994bbb65103f99
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-09-11 10:11:33 -0300

    Criando serviço de service discovery

---
+++ src/test/java/br/com/a2like/poceurekaserver/PocEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.a2like.poceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
